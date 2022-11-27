55ff34ba7ddc4fe4ae39bccefa93f617fb06e62f
Dmitriy <kravchenkodmitriy38@gmail.com>
2022-11-27 18:44:52 +0300

    Initial commit

---
+++ service/src/main/java/ru/netology/MyService.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import org.example.Db;
+import org.example.DbSetting;
+import org.example.MyEntity;
+
+import java.util.UUID;
+
+public class MyService {
+
+
+    private DbSetting dbSetting = new DbSetting("name", "password");
+    private String name = "myService";
+    private Db db = new Db(dbSetting);
+
+    public String getName() {
+        return name;
+    }
+
+    public MyEntity setMyEntity(MyEntity myEntity) {
+        myEntity.setId(UUID.randomUUID());
+        db.setMyEntity(myEntity);
+        return myEntity;
+    }
+
+    public MyEntity getMyEntity() {
+        return db.getMyEntity();
+    }
+
+}
